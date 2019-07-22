5ca2766ff008a0f8cba006d11eaa41f23f185625
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-22 16:02:43 -0700

    refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,16 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params.require(:post).permit(:title, :description))
+		@post = Post.new(params.require(:post).permit(:title, :description))
+		# @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title))
+		@post.update(params.require(:post).permit(:title))
+		# @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
@@ -27,9 +29,15 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 
+	private
+	
+	# def post_params
+	# 	params.require(:post).permit(:title, :description)
+	# end
+
 	# private
 
-	# def post_params(*args)
-	# 	params.require(:post).permit(*args)
-	# end
+	def post_params(*args)
+		params.require(:post).permit(*args)
+	end
 end
